e24c8a29bad423776f01e3bdbe341e0a0aba13fe
Michael Albanese <38363373+MichaelAlbanese@users.noreply.github.com>
2020-02-13 10:26:31 -0600

    Completed

---
+++ core-background/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  core-background
+//
+//  Created by Michael Albanese on 6/28/13.
+//  Copyright (c) 2013 Michael Albanese. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
